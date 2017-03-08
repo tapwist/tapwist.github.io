f75502cd81fa003b95c5ceab90505ebf00758545
tapwist <vityawoolfik@mail.ru>
2017-03-08 12:49:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+var x = prompt("1st string:");
+var y = prompt("2nd string:");
 
+x = x.toLowerCase();
+y = y.toLowerCase();
+
+x = x.replace(/\s+/g,'');
+y = y.replace(/\s+/g,'');
+
+
+console.log(x, y);
