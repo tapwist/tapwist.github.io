a1790c938b9c0b1a1ed15f3785153d3508f86220
tapwist <vityawoolfik@mail.ru>
2017-03-10 12:49:16 +0300

    Update 1.2.js

--- 1.2/1.2.js
+++ 1.2/1.2.js
@@ -1,5 +1,9 @@
-setTimeout(function(){
+for (var i = 0; i < 10; i++) {
+  setTimeout(console.log(i), 1000);
+}
+
+/*setTimeout(function(){
   for (var i = 0; i < 10; i++){
     console.log(i);
   }
-}, 1000);
+}, 1000);*/
