33131f30aa22f0b13abba8e74c98b593eddcb9b7
tapwist <vityawoolfik@mail.ru>
2017-03-20 19:40:04 +0300

    Update app.js

--- 2.2/app.js
+++ 2.2/app.js
@@ -1,4 +1,4 @@
-var a = prompt('первый массив').split(', ');
+/*var a = prompt('первый массив').split(', ');
 var b = prompt('второй массив').split(', ');
 var c = [];
 for (var i = 0; i < a.length; i++){
@@ -6,4 +6,15 @@ for (var i = 0; i < a.length; i++){
   if (index >= 0) c.push(a[i]);
 }
 console.log(a, b);
-alert(c);
+alert(c);*/
+
+function intersection(set1, set2) {
+  var temp = {};
+  set1.forEach(function(item){
+    temp[item] = true;
+  });
+  return set2.filter(function(item) {
+    return temp[item];
+  })
+}
+console.log(intersection([1, 3, 5, 7, 9],[1,2,3,4]));
